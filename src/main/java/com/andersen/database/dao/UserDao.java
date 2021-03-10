28927208515fa30a15f4e78ab2303a7eab299888
Ilsur <harpegnathos.venator@gmail.com>
2021-03-10 11:34:34 +0300

    Added DAO Interface

---
+++ src/main/java/com/andersen/database/dao/UserDao.java
@@ -0,0 +1,18 @@
+package com.andersen.database.dao;
+
+import com.andersen.database.entity.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    public void save(User user);
+
+    public User get(Integer id);
+
+    public void update(User user);
+
+    public void delete(Integer id);
+
+    public List<User> getAllUsers();
+}
